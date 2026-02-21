01b0bd8c3a094c49af8df83555ad7ad8fc150d4c
pygonza <andro.gonzs2003@gmail.com>
2026-02-21 12:08:31 -0400

    first commit

---
+++ fundamentos.py
@@ -0,0 +1,52 @@
+print("Hello, World!")
+#variables
+name = "Alice"
+age = 30 
+last_name = "Martinez"
+
+#Operadores matematicos
+number1 = 10
+number2 = 5
+
+#suma
+sum_result = number1 + number2
+
+print("la suma es:", sum_result)
+print(f"la suma es: {sum_result}")
+
+#resta
+rest_result = number1 - number2 
+print(f"la resta es: {rest_result}")
+
+#multiplicacion
+mult_result = number1 * number2
+print(f"la multiplicacion es: {mult_result}")
+
+#division
+div_result = number1 / number2
+print(f"la division es: {div_result}")
+
+#potenciacion
+pow_result = number1 ** number2
+print(f"la potenciacion es: {pow_result}")
+
+#modulo
+mod_result = number1 % number2
+print(f"El modulo es: {mod_result}")
+
+#Operadores de comparacion 
+print(number1 > number2) #True
+mayor_que = number1 > number2
+menor_que = number1 < number2
+igual_que = number1 == number2 
+diferente_que = number1 >= number2
+menor_igual_que = number1 <= number2 
+
+#condicional simple
+number1 = 10
+number2 = 5
+if number1 > number2:
+    print(f"{number1} es mayor que {number2}")
+else:
+    print(f"{number1} no es mayor que {number2}")
+    
